d0b2bef66ea67e5d6fc4522837064ab5c6373ce5
jacobbubu <rong.shen@gmail.com>
2020-04-22 19:56:49 +0800

    fix: serialization supports Error object

--- src/json-serializer.ts
+++ src/json-serializer.ts
@@ -1,17 +1,46 @@
 import * as pull from 'pull-stream'
 import split from '@jacobbubu/pull-split'
 import through from '@jacobbubu/pull-through'
+import { CommandType, EventIndex } from './event'
 
 const toBeTruthy = (d: any) => !!d
 
 const serialize = function () {
   return through(function (data) {
+    if (
+      Array.isArray(data) &&
+      (data[EventIndex.EventType] === CommandType.EndOrError ||
+        data[EventIndex.EventType] === CommandType.Data)
+    ) {
+      const e = data[EventIndex.Payload]
+      // transform the Error object to plain array
+      if (e instanceof Error) {
+        data[EventIndex.Payload] = ['__ERROR__', e.message]
+      }
+    }
     this.queue(JSON.stringify(data) + '\n')
   })
 }
 
 const parse = function () {
-  return pull(split(), pull.filter(toBeTruthy), pull.map(JSON.parse))
+  return pull(
+    split(),
+    pull.filter(toBeTruthy),
+    pull.map((data) => {
+      const parsed = JSON.parse(data)
+      if (
+        Array.isArray(parsed) &&
+        (parsed[EventIndex.EventType] === CommandType.EndOrError ||
+          parsed[EventIndex.EventType] === CommandType.Data)
+      ) {
+        const e = parsed[EventIndex.Payload]
+        if (Array.isArray(e) && e[0] === '__ERROR__') {
+          parsed[EventIndex.Payload] = new Error(e[1])
+        }
+      }
+      return parsed
+    })
+  )
 }
 
 export { serialize }
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,5 +1,6 @@
 import * as pull from 'pull-stream'
-import { Plex, Channel } from '../src'
+import { Plex, Channel, wrap } from '../src'
+import through from '@jacobbubu/pull-through'
 
 describe('basic', () => {
   it('constructor', () => {
@@ -61,5 +62,108 @@ describe('basic', () => {
     })
 
     pull(plex1, plex2, plex1)
+  })
+
+  it('serialization', (done) => {
+    const plex1 = new Plex({ from: 'p1' })
+    const plex2 = new Plex({ from: 'p2' })
+
+    const a = plex1.createChannel('a')
+
+    let result1: any[]
+    let result2: any[]
+
+    const hasDone = () => {
+      if (result1 && result2) {
+        expect(plex1.peerMeta).toEqual(plex2.meta)
+        expect(plex2.peerMeta).toEqual(plex1.meta)
+        expect(result1).toEqual([4, 5, 6])
+        expect(result2).toEqual([1, 2, 3])
+        done()
+      }
+    }
+
+    pull(pull.values([1, 2, 3]), a.sink)
+    pull(
+      a.source,
+      pull.collect((err, ary) => {
+        expect(err).toBeFalsy()
+        result1 = ary
+        hasDone()
+      })
+    )
+
+    plex2.on('channel', (channel: Channel) => {
+      pull(
+        channel.source,
+        pull.collect((err, ary) => {
+          expect(err).toBeFalsy()
+          result2 = ary
+        })
+      )
+      pull(pull.values([4, 5, 6]), channel.sink)
+    })
+
+    const wrappedPlex = wrap(plex1)
+    pull(wrappedPlex, wrap(plex2), wrappedPlex)
+  })
+
+  it('error serialization', (done) => {
+    const plex1 = new Plex({ from: 'p1' })
+    const plex2 = new Plex({ from: 'p2' })
+
+    const a = plex1.createChannel('a')
+
+    let result1: any[]
+    let result2: any[]
+
+    const hasDone = () => {
+      if (result1 && result2) {
+        expect(plex1.peerMeta).toEqual(plex2.meta)
+        expect(plex2.peerMeta).toEqual(plex1.meta)
+        expect(result1[0]).toBe(4)
+        expect(result1[1]).toBeInstanceOf(Error)
+        expect(result1[1].message).toBe('error')
+
+        expect(result2).toEqual([1])
+        done()
+      }
+    }
+
+    pull(
+      pull.values([1, 2, 3]),
+      through(function (d) {
+        if (d === 2) {
+          this.emit('error', new Error('error'))
+        } else {
+          this.queue(d)
+        }
+      }),
+      a.sink
+    )
+    pull(
+      a.source,
+      pull.collect((err, ary) => {
+        expect(err).toBeFalsy()
+        result1 = ary
+        hasDone()
+      })
+    )
+
+    plex2.on('channel', (channel: Channel) => {
+      pull(
+        channel.source,
+        pull.collect((err, ary) => {
+          expect(err).toBeTruthy()
+          expect(err).toBeInstanceOf(Error)
+          expect((err as Error).message).toBe('error')
+          result2 = ary
+        })
+      )
+      pull(pull.values([4, new Error('error')]), channel.sink)
+    })
+
+    const wrappedPlex = wrap(plex1)
+    pull(wrappedPlex, wrap(plex2), wrappedPlex)
   })
 })
